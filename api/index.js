053bfe77a7dfa3148cc5fb6d58b74700a84a7fb4
javito96 <javier.cornejo.niederle@gmail.com>
2022-07-06 10:03:25 -0300

    modificado

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at PORT'); // eslint-disable-line no-console
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
